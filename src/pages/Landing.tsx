d95bd3d303e728913b80ef198e0fbb2d9e206cd4
bootaeng <tgwon0947@gmail.com>
2025-09-06 17:05:38 +0900

    refactor: update main with latest work (squash)

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,16 @@
 import { LoginButton } from '@/components/home_page/LoginButton';
 import { Container, Text } from '@/style/CommonStyle';
 import { useNavigate } from 'react-router-dom';
-
+import { Logo } from './Landing.styled';
+import LogoImg from '@/asset/LogoImg.png';
 
 const Landing = () => {
   const navigte = useNavigate();
 
   return (
     <Container>
-      <Text onClick={() => navigte('/home')}>안녕하세요?</Text>
+      <Logo alt="모밋 로고" src={LogoImg} onClick={() => navigte('/home')}></Logo>
+      <Text>MOMEET</Text>
       <LoginButton onClick={() => navigte('/login')} />
     </Container>
   );
